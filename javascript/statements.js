58f4de295d2f239ec6316eef1f26aa3293f4b1ee
tpark16 <tpark16@stlawu.edu>
2020-01-29 13:42:42 -0500

    javascript

---
+++ javascript/statements.js
@@ -0,0 +1,26 @@
+// Statement syntax in JavaScript
+
+// Constants
+const pi = 3.14;
+
+// Printing
+console.log(pi);
+
+// For loop
+for (let x = 0; x < 3; x++) {
+  console.log(x);
+}
+
+// While loop
+let x = 0;
+while (x < 3) {
+  console.log(x);
+  x += 1;
+}
+
+// Conditional
+if (x === 3) {
+  console.log('x is now 3');
+} else {
+  console.log(`x is now ${x}`);
+}
