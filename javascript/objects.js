0ae82322191508d7743e5879e4b7af46dd7ca78d
tpark16 <tpark16@stlawu.edu>
2020-02-03 13:07:22 -0500

    object

--- javascript/objects.js
+++ javascript/objects.js
@@ -2,3 +2,21 @@
 
 // Object literal
 const point = {x:1, y:2}
+
+// Changing properties
+porint x = 3;
+
+// Nested object literal
+const circle = {
+  center: point,
+  radius: 4
+};
+
+// Access subproperties
+console.log(circle.center.x);
+
+// undefined subproperties
+console.log(circle.color);
+
+circle.color = 'blue';
+console.log(circle);
